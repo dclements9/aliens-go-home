30d9901f18f1d3578fc1b0e800e10f70d6ef89d4
Dylan Clements <dclements@protonmail.com>
2021-01-07 11:37:30 -0500

    windows->window & serviceWorker comment

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Game from './containers/Game';
 import reducer from './reducers';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './serviceWorker';
 
 const store = createStore(
   reducer, /* preloadedState, */
-  windows.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
 
@@ -21,7 +21,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
